c731521ad0d8a110631f19906271e9c52768dbec
alex <alex@activewidgets.com>
2019-05-07 00:28:50 +0200

    use html 'framework'

--- index.js
+++ index.js
@@ -3,45 +3,11 @@ import {Component} from 'inferno';
 import {cloneVNode} from 'inferno-clone-vnode';
 import {createElement} from 'inferno-create-element';
 import {hydrate} from 'inferno-hydrate';
-import framework from '@activewidgets/frameworks/inferno';
-import {grid} from '@activewidgets/grid';
+import framework from '@activewidgets/frameworks/html';
+import * as pkg from '@activewidgets/grid';
 
+const {build, tags} = framework({Component, createElement, cloneVNode, hydrate});
 
-let {component} = framework({Component, createElement, cloneVNode});
+export const {Grid, Row, View, Scrollbars} = build(pkg);
+export {createElement as h, tags};
 
-
-function mount(source, props){
-
-    let AxComponent = component(source),
-        target = (typeof props.el == 'string') ? document.querySelector(props.el) : props.el,
-        placeholder = document.createElement('div'),
-        container = document.createElement('div'),
-        instance;
-
-    if (target) {
-        target.parentNode.replaceChild(placeholder, target);
-        container.appendChild(target);
-    }
-
-    class Proxy extends Component {
-        constructor(...args){
-            super();
-            if (target) {
-                placeholder.parentNode.replaceChild(target, placeholder);
-            }
-            instance = new AxComponent(...args);
-            return instance;
-        }
-    }
-
-    hydrate(createElement(Proxy, props), container);
-    instance.el = target || container.firstChild;
-    return instance;
-};
-
-
-export class Grid {
-    constructor(props){
-        return mount(grid, props);
-    }
-}
