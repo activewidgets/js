7c3d9bb0ec0a60565a8ca8c572bed5a40d3b4516
alex <alex@activewidgets.com>
2019-12-13 21:36:41 +0100

    move storybook, examples, tests to separate packages

--- examples/basic.js
+++
@@ -1,8 +0,0 @@
-
-import {Grid} from '../';
-import {columns, rows} from './data/customers.js';
-
-export default () => {
-    let obj = new Grid({rows, columns});
-    return obj.mount();
-};
--- storybook/e2e.js
+++
@@ -1,7 +0,0 @@
-
-import {storiesOf} from '@storybook/html';
-import basic from '../examples/basic.js';
-
-storiesOf('Tests', module)
-
-    .add('basic', basic);
