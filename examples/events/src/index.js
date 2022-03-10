a50f46c1142d9406f7e715d39034d88da295c073
alex <alex@activewidgets.com>
2022-03-10 13:02:02 +0100

    fix events example

--- examples/events/src/index.js
+++ examples/events/src/index.js
@@ -9,9 +9,19 @@ import { columns, rows } from "@activewidgets/examples/data";
 import './styles.css';
 
 
-function onMouse({row}){
-    alert(`row ${row.key} clicked!`);
+let gridAPI;
+
+function onInit(api){
+    gridAPI = api;
 }
 
 
-mount('ax-datagrid', {columns, rows, onMouse});
+function onClick(event){
+    let cell = gridAPI.cellFromElement(event.target); 
+    if (cell && cell.section === 'main') {
+        alert(`row ${cell.row.index + 1} clicked!`);
+    }
+}
+
+
+mount('ax-datagrid', {columns, rows, onInit, onClick});
